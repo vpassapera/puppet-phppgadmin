e430302ddad48dcdbc201857107c804805e61bfa
Victor Passapera <vpassapera@gmail.com>
2015-02-17 01:37:19 -0800

    Upgraded to apache 2.4+

--- manifests/init.pp
+++ manifests/init.pp
@@ -34,53 +34,59 @@
 # Copyright 2013 Shantanu Pavgi.
 #
 class phppgadmin (
-  $install_apache       = $phppgadmin::params::install_apache,
-  $install_postgres     = $phppgadmin::params::install_postgres,
-  $db_host              = $phppgadmin::params::db_host,
-  $db_port              = $phppgadmin::params::db_port,
-  $http_conf_file       = $phppgadmin::params::http_conf_file
+    $install_apache       = $phppgadmin::params::install_apache,
+    $install_postgres     = $phppgadmin::params::install_postgres,
+    $db_host              = $phppgadmin::params::db_host,
+    $db_port              = $phppgadmin::params::db_port
 ) inherits phppgadmin::params {
 
-  $phppgadmin_conf_file = $phppgadmin::params::phppgadmin_conf_file
-  $phppgadmin_package   = $phppgadmin::params::phppgadmin_package
+    $http_conf_file       = $phppgadmin::params::http_conf_file
+    $phppgadmin_package   = $phppgadmin::params::phppgadmin_package
+    $phppgadmin_conf_file = $phppgadmin::params::phppgadmin_conf_file
 
-  if $install_postgres == true {
-    class {'postgresql::server': }
-  }
-
-  if $install_apache == true {
-    class {'apache': }
-    class {'apache::mod::php': }
-    package{$phppgadmin_package:
-      ensure  => installed,
-      require => Class['apache::mod::php'],
+    if $install_postgres == true {
+        class { 'postgresql::server': }
     }
-  }
-  else {
-    package{$phppgadmin_package:
-      ensure => installed,
+
+    if $install_apache == true {
+        class { 'apache': }
+        class { 'apache::mod::php': }
+        package{ $phppgadmin_package:
+            ensure  => installed,
+            require => Class['apache::mod::php'],
+        }
     }
-  }
+    else {
+        package{ $phppgadmin_package:
+            ensure => installed,
+        }
+    }
 
-  file{$http_conf_file:
-    ensure  => present,
-    mode    => '0644',
-    content => template($http_conf_template_file),
-    require => Package[$phppgadmin_package],
-  }
+    file{ $http_conf_file:
+        ensure  => present,
+        mode    => '0644',
+        content => template($http_conf_template_file),
+        require => Package[$phppgadmin_package],
+    }
 
-  file_line{'phppgadmin_conf_file_host':
-    path    => $phppgadmin_conf_file,
-    match   => "\\t\\\$conf\\['servers'\\]\\[0\\]\\['host'\\] = '.*';$",
-    line    => "\t\$conf['servers'][0]['host'] = '${db_host}';",
-    require => Package[$phppgadmin_package],
-  }
+    file_line{ 'phppgadmin_conf_file_host':
+        path    => $phppgadmin_conf_file,
+        match   => "\\t\\\$conf\\['servers'\\]\\[0\\]\\['host'\\] = '.*';$",
+        line    => "\t\$conf['servers'][0]['host'] = '${db_host}';",
+        require => Package[$phppgadmin_package],
+    }
 
+    file_line{'phppgadmin_conf_file_port':
+        path    => $phppgadmin_conf_file,
+        match   => "\\t\\\$conf\\['servers'\\]\\[0\\]\\['port'\\] = \\d+;$",
+        line    => "\t\$conf['servers'][0]['port'] = ${ db_port };",
+        require => Package[$phppgadmin_package],
+    }
 
-  file_line{'phppgadmin_conf_file_port':
-    path    => $phppgadmin_conf_file,
-    match   => "\\t\\\$conf\\['servers'\\]\\[0\\]\\['port'\\] = \\d+;$",
-    line    => "\t\$conf['servers'][0]['port'] = ${db_port};",
-    require => Package[$phppgadmin_package],
-  }
+    file_line{'phppgadmin_conf_file_security':
+        path    => $phppgadmin_conf_file,
+        match   => "\\t\\\$conf\\['extra_login_security'\\] = false;$",
+        line    => "\t\$conf['extra_login_security'] = true;",
+        require => Package[$phppgadmin_package],
+    }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -20,25 +20,25 @@
 #
 class phppgadmin::params {
 
-  $install_apache     = false   # if true, default apache install using puppetlabs-apache
-  $install_postgres   = false   # if true, default postgresql install using puppet-postgresql
-  $db_host            = ''      # default - unix socket connection
-  $db_port            = '5432'  #
-  
-  
-  case $::operatingsystem {
-    'RedHat', 'CentOS', 'Fedora': {
-      $phppgadmin_package = 'phpPgAdmin'
-      $phppgadmin_conf_file = '/etc/phpPgAdmin/config.inc.php'
-      $http_conf_file = '/etc/httpd/conf.d/phpPgAdmin.conf'
-      $http_conf_template_file = 'phppgadmin/CentOS/phpPgAdmin.conf.erb'
+    $install_apache     = false   # if true, default apache install using puppetlabs-apache
+    $install_postgres   = false   # if true, default postgresql install using puppet-postgresql
+    $db_host            = ''      # default - unix socket connection
+    $db_port            = '5432'  #
+
+
+    case $::operatingsystem {
+        'RedHat', 'CentOS', 'Fedora': {
+            $phppgadmin_package = 'phpPgAdmin'
+            $phppgadmin_conf_file = '/etc/phpPgAdmin/config.inc.php'
+            $http_conf_file = '/etc/httpd/conf.d/phpPgAdmin.conf'
+            $http_conf_template_file = 'phppgadmin/CentOS/phpPgAdmin.conf.erb'
+        }
+        'Debian', 'Ubuntu': {
+            $phppgadmin_package = 'phppgadmin'
+            $phppgadmin_conf_file = '/etc/phppgadmin/config.inc.php'
+            $http_conf_file = '/etc/apache2/conf.d/phppgadmin'
+            $http_conf_template_file = 'phppgadmin/Ubuntu/phppgadmin.conf.erb'
+        }
+        default: { fail("Unsupported platform: ${::osfamily}/${::operatingsystem}") }
     }
-    'Debian', 'Ubuntu': {
-      $phppgadmin_package = 'phppgadmin'
-      $phppgadmin_conf_file = '/etc/phppgadmin/config.inc.php'
-      $http_conf_file = '/etc/apache2/conf.d/phppgadmin'
-      $http_conf_template_file = 'phppgadmin/Ubuntu/phppgadmin.conf.erb'
-    }
-    default: { fail("Unsupported platform: ${::osfamily}/${::operatingsystem}") }
-  }
 }
